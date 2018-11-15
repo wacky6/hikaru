ef81172811a506aaa9609378d2fe9afefb376ada
Jiewei Qian <qjw@wacky.one>
2018-11-16 01:49:04 +1100

    chore: add exports

---
+++ exports.js
@@ -0,0 +1,12 @@
+const {
+    DanmakuStream,
+    HighAvailabilityDanmakuStream
+} = require('./lib/danmaku')
+
+const API = require('./lib/bili-api')
+
+module.exports = {
+    DanmakuStream,
+    HighAvailabilityDanmakuStream,
+    API
+}
