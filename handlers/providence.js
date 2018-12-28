bea6e411a54af563d8583d059f8ab5e40de7b11c
Jiewei Qian <qjw@wacky.one>
2018-12-28 16:49:57 +0800

    providence: add live history and host info update

--- handlers/providence.js
+++ handlers/providence.js
@@ -9,6 +9,7 @@ const AmqpSubscriber = require('../lib/amqp-subscribe')
 const parseDanmaku = require('../lib/providence-danmaku-parser')
 const { MongoDump } = require('../lib/_mongo')
 const moment = require('moment-timezone')
+const { autoRetry, getRoomInfo, getRoomUser } = require('../lib/bili-api')
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -120,6 +121,34 @@ function upsertWithRetry(dbConn, coll, id, updateOperator) {
     return updateFn()
 }
 
+async function updateHostInfo(dbConn, canonicalRoomId) {
+    const roomInfo = await autoRetry(getRoomInfo, canonicalRoomId)
+    const { roomId } = roomInfo
+
+    const userInfo = await autoRetry(getRoomUser, roomId)
+    const { uid } = userInfo
+
+    dbConn && dbConn.getConn().then(
+        conn => conn.db().collection('user').updateOne(
+            { _id: uid },
+            { $set: { ...roomInfo, ...userInfo },
+              $currentDate: { _lastModified: true },
+            },
+            { upsert: true }
+        ).catch(dbConn.errorHandler)
+    )
+
+    if (roomInfo.liveStatus === 1) {
+        dbConn && dbConn.getConn().then(
+            conn => conn.db().collection('providence_live_history').insertOne({
+                roomId,
+                liveStartsAt: roomInfo.liveStartsAt,
+                title: roomInfo.title,
+            }).catch(dbConn.errorHandler)
+        )
+    }
+}
+
 module.exports = {
     yargs: yargs => injectOptions(yargs, globalOpts, subscribeOpts, databaseOpts)
 
@@ -145,6 +174,7 @@ module.exports = {
             ret => ret || console.error('mongo: connection not established yet, continue anyway.')
         )
 
+        // deal with messages for statistical purposes
         sub.on('message', async _msg => {
             const msg = JSON.parse(_msg)
 
@@ -168,6 +198,36 @@ module.exports = {
                 { time: statTime, uid: parsed.uid, roomId: msg.roomId },
                 updateUser
             )
+        })
+
+        // deal with command messages
+        sub.on('message', async _msg => {
+            const msg = JSON.parse(_msg)
+
+            const HOST_INFO_UPDATE_DELAY = 180 * 1000    // 3 min
+
+            if (msg.cmd === 'READY') {    // host is going on line
+                setTimeout(_ => updateHostInfo(dbConn, msg.roomId), HOST_INFO_UPDATE_DELAY)
+            }
+            if (msg.cmd === 'PREPARING') {    // host is going off live
+                setTimeout(_ => updateHostInfo(dbConn, msg.roomId), HOST_INFO_UPDATE_DELAY)
+            }
+            if (msg.cmd === 'WARNING') {    // admin is visiting, must be something of interest
+                dbConn && dbConn.getConn().then(
+                    conn => conn.db().collection('user').updateOne(
+                        { roomId: msg.roomId },
+                        {
+                            $inc: { total_warnings: 1 },
+                            $push: {
+                                'warnings': {
+                                    $each: [{ time: new Date(msg._rxTime), text: msg.msg }],
+                                    $slice: -10,
+                                },
+                            }
+                        },
+                    ).catch(dbConn.errorHandler)
+                )
+            }
         })
     }
 }
